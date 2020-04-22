d6da0da3eb1ed9e42c12105c8762fd829ff98710
Garrett Tolbert <garrett1.tolbert@gmail.com>
2020-04-22 19:45:21 -0400

    added empty state

--- src/components/AllActivity.js
+++ src/components/AllActivity.js
@@ -4,6 +4,7 @@ import { MainContext } from '../context/MainContext';
 import { ThemeContext } from '../context/ThemeContext';
 import { ActivityItem } from './Activity';
 import { Link } from 'react-router-dom';
+import NotFound from './NotFound';
 
 const AllActivity = () => {
     const { transactions } = useContext(MainContext);
@@ -34,28 +35,46 @@ const AllActivity = () => {
                     </h3>
                 </div>
             </Link>
-            <h1
-                style={{
-                    color: isDark
-                        ? darkTheme.textPrimary
-                        : lightTheme.textPrimary,
-                }}
-            >
-                All Activity
-            </h1>
-            <div className='aa-container'>
-                {transactions.map((item, idx) => (
-                    <ActivityItem
-                        key={idx}
-                        home={false}
-                        arrayIndex={idx}
-                        id={item.id}
-                        amount={item.amount}
-                        label={item.label}
-                        transactionType={item.transactionType}
-                    />
-                ))}
-            </div>
+            {transactions.length === 0 ? (
+                <>
+                    <NotFound />
+                    <h1
+                        className='notFound'
+                        style={{
+                            color: isDark
+                                ? darkTheme.textPrimary
+                                : lightTheme.textPrimary,
+                        }}
+                    >
+                        No activity to show
+                    </h1>
+                </>
+            ) : (
+                <>
+                    <h1
+                        style={{
+                            color: isDark
+                                ? darkTheme.textPrimary
+                                : lightTheme.textPrimary,
+                        }}
+                    >
+                        All Activity
+                    </h1>
+                    <div className='aa-container'>
+                        {transactions.map((item, idx) => (
+                            <ActivityItem
+                                key={idx}
+                                home={false}
+                                arrayIndex={idx}
+                                id={item.id}
+                                amount={item.amount}
+                                label={item.label}
+                                transactionType={item.transactionType}
+                            />
+                        ))}
+                    </div>
+                </>
+            )}
         </div>
     );
 };
